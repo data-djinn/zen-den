509e8223e3d9989722c32eed97e491d0d6b3eeed
data-djinn <data-djinn@pm.me>
2022-11-02 19:27:30 +0000

    reformat

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,12 +8,11 @@
 
     # Feel free to split up your configuration and import pieces of it here.
   ];
-  
-  xsession.enable = true;
+
   home = {
     username = "djinn";
     homeDirectory = "/home/djinn";
-    
+
     keyboard = {
       layout = "us";
       variant = "dvorak";
@@ -25,7 +24,6 @@
       curl
       gh
       librewolf
-      redshift
       pfetch
       obsidian
       python3
@@ -33,19 +31,9 @@
     ];
   };
 
-  environment.variables = {
-    EDITOR = "nvim";
-    VISUAL = "nvim";
-    TERMINAL = "alacritty";
-  };
-
   programs = {
     bash = {
       enable = true;
-
-      bashrcExtra = "
-        export EDITOR=nvim
-	    export TERMINAL=alacritty";  # for i3 terminal launch
 
       initExtra = "pfetch";
       shellAliases = {
@@ -57,9 +45,9 @@
         "..." = "cd ../..";
         count = "find . -type f | wc -l";
         cpv = "rsync -ah --info=progress2";
-	    gs = "git fetch && git status";
-	    ga = "git fetch && git add";
-	    gc = "git commit -m";
+        gs = "git fetch && git status";
+        ga = "git fetch && git add";
+        gc = "git commit -m";
       };
     };
 
@@ -78,39 +66,39 @@
       withPython3 = true;
       plugins = with pkgs.vimPlugins; [
         vim-nix
-	    jedi-vim
-	    vim-airline
-	    vim-airline-themes
-	    molokai
-	    vim-commentary
-	    indentLine
+        jedi-vim
+        vim-airline
+        vim-airline-themes
+        molokai
+        vim-commentary
+        indentLine
       ];
       extraConfig = "
-        set fileencoding=utf-8
+      set fileencoding=utf-8
 
-        set backspace=indent,eol,start
+      set backspace=indent,eol,start
 
-        set tabstop=4
-        set softtabstop=0
-        set shiftwidth=4
-        set expandtab
+      set tabstop=4
+      set softtabstop=0
+      set shiftwidth=4
+      set expandtab
 
-        set hlsearch
-        set incsearch
-        set ignorecase
-        set smartcase
+      set hlsearch
+      set incsearch
+      set ignorecase
+      set smartcase
 
-        syntax on
-        set ruler
-        set number
+      syntax on
+      set ruler
+      set number
 
-        colorscheme molokai
+      colorscheme molokai
 
-        set wildmenu
+      set wildmenu
 
-        let g:indentLine_enabled = 1
-        let g:indendLink_concealcursor = ''
-        let g:indentLine_faster = 1
+      let g:indentLine_enabled = 1
+      let g:indendLink_concealcursor = ''
+      let g:indentLine_faster = 1
       ";
     };
 
@@ -128,12 +116,23 @@
 
     rbw.enable = true;  # bitwarden cli client TODO: self-host
   };
+
+    # reduce blue light after sunset
+    services.redshift = {
+      enable = true;
+      provider = "geoclue2";
+    };
   
   manual.html.enable = true;  # view with `home-manager-help`
 
   # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
-
+  systemd.user = {
+    startServices = "sd-switch";
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+      TERMINAL = "alacritty";
+    };
+  };
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "22.05";
 }
