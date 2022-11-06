b1162dcf8d0a0323a5451c3c21635e7d31f10e8f
data-djinn <data-djinn@pm.me>
2022-11-06 13:11:36 -0500

    add local timezone-setting daemon based on geoclue2

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -166,6 +166,7 @@ in
   # Enable automatic location
   services.geoclue2.enable = true;  # TODO: restrict to specific users
   location.provider = "geoclue2";
+  services.localtimed.enable = true;
 
   console.keyMap = "dvorak";
 
