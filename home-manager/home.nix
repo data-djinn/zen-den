fdd13f660d96a12be129d1464b59565f0ea57381
data-djinn <data-djinn@pm.me>
2022-11-13 07:53:40 -0500

    mv vim config to init.vim & readFile

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -135,55 +135,16 @@
         vim-airline
       ];
 
-      extraConfig = ''
-        set fileencoding=utf-8
+      extraConfig = builtins.readFile ./init.vim;
 
-        set expandtab
-        set number
-        set shiftwidth=4
-        set smartcase
-        set tabstop=4
-
-        set backspace=indent,eol,start
-
-        set softtabstop=0
-
-        set hlsearch
-        set incsearch
-        set ignorecase
-        set smartcase
-
-        syntax on
-
-        colorscheme molokai
-
-        set wildmenu
-
-        "" always show status bar
-        set laststatus=2
-
-        "" center screen on search match
-        nnoremap n nnzzzv
-        nnoremap N Nzzzv
-
-        let g:indentLine_enabled = 1
-        let g:indentLine_faster = 1
-
-        command! FixWhitespace :%s/\s\+$//e
-
-        augroup vimrc-sync-fromstart
-          autocmd!
-          autocmd BufEnter * :syntax sync maxlines=200
-        augroup END
-
-        set autoread
-      '';
       extraPackages = with pkgs; [
         (python3.withPackages (ps: with ps; [
           black
           flake8
         ]))
         nodejs  # required for coc
+        haskell-language-server
+        rnix-lsp
       ];
       extraPython3Packages = (ps: with ps; [
         jedi
