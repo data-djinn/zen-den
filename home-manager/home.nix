49c36288cb3b00ba7a83ed48e9f0fa27d25661d4
data-djinn <data-djinn@pm.me>
2022-11-13 08:28:59 -0500

    mv neovim config to seperate file

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,8 +5,7 @@
   imports = [
     # If you want to use home-manager modules from other flakes (such as nix-colors), use something like:
     # inputs.nix-colors.homeManagerModule
-
-    # Feel free to split up your configuration and import pieces of it here.
+    ./neovim/neovim.nix
   ];
 
   home = {
@@ -67,90 +66,6 @@
       };
     };
 
-    # TODO: move to seperate flake!
-    neovim = {
-      enable = true;
-      withPython3 = true;
-      coc = {
-        enable = true;
-        settings = {
-          "suggest.noselect" = true;
-          "suggest.enablePreview" = true;
-          "suggest.enablePreselect" = true;
-          "suggest.disableKind" = true;
-          languageserver = {
-            haskell = {
-              command = "haskell-language-server-wrapper";
-              args = [ "--lsp"  ];
-              rootPatterns = [
-                "*.cabal"
-                "stack.yaml"
-                "cabal.project"
-                "package.yaml"
-                "hie.yaml"
-              ];
-              filetypes = [ "haskell" "lhaskell" ];
-            };
-            nix = {
-              command = "rnix-lsp";
-              filetypes = ["nix"];
-            };
-          };
-        };
-      };
-
-      viAlias = true;
-      vimAlias = true;
-      plugins = with pkgs.vimPlugins; [
-        # general
-        indentLine  # shows line
-        vim-commentary  # `gcc` to comment out/in a line; `gc` for motion/viz
-                        # use e.g. `:97,98Commentary` to specify a range
-        ale  # async lint engine
-        coc-nvim
-        coc-python
-
-        # elm extensions
-        elm-vim
-
-        # haskell extensions
-        neco-ghc
-        vim2hs
-
-        # git extensions
-        vim-fugitive
-        # vim-gitgutter
-
-          # nix extensions
-          vim-nix
-
-        # python extensions
-        jedi-vim
-
-        # scala extensons
-        vim-scala
-
-        # theme
-        molokai
-        vim-airline
-      ];
-
-      extraConfig = builtins.readFile ./init.vim;
-
-      extraPackages = with pkgs; [
-        (python3.withPackages (ps: with ps; [
-          black
-          flake8
-        ]))
-        nodejs  # required for coc
-        haskell-language-server
-        rnix-lsp
-      ];
-      extraPython3Packages = (ps: with ps; [
-        jedi
-      ]);
-    };
-
     git = {
       enable= true;
       userName = "data-djinn";
@@ -165,7 +80,6 @@
 
     rbw.enable = true;  # bitwarden cli client TODO: self-host
   };
-  xdg.configFile."nvim/coc-settings.json".text = builtins.readFile ./coc-settings.json;
 
     # reduce blue light after sunset
     services.redshift = {
---
+++ home-manager/neovim/neovim.nix
@@ -0,0 +1,87 @@
+{pkgs, inputs, system, ... }:
+{
+  programs.neovim = {
+    enable = true;
+    withPython3 = true;
+    coc = {
+      enable = true;
+      settings = {
+        "suggest.noselect" = true;
+        "suggest.enablePreview" = true;
+        "suggest.enablePreselect" = true;
+        "suggest.disableKind" = true;
+        languageserver = {
+          haskell = {
+            command = "haskell-language-server-wrapper";
+            args = [ "--lsp"  ];
+            rootPatterns = [
+              "*.cabal"
+              "stack.yaml"
+              "cabal.project"
+              "package.yaml"
+              "hie.yaml"
+            ];
+            filetypes = [ "haskell" "lhaskell" ];
+          };
+          nix = {
+            command = "rnix-lsp";
+            filetypes = ["nix"];
+          };
+        };
+      };
+    };
+
+    viAlias = true;
+    vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      # general
+      indentLine  # shows line
+      vim-commentary  # `gcc` to comment out/in a line; `gc` for motion/viz
+                      # use e.g. `:97,98Commentary` to specify a range
+      ale  # async lint engine
+      coc-nvim
+      coc-python
+
+      # elm extensions
+      elm-vim
+
+      # haskell extensions
+      neco-ghc
+      vim2hs
+
+      # git extensions
+      vim-fugitive
+      # vim-gitgutter
+
+        # nix extensions
+        vim-nix
+
+      # python extensions
+      jedi-vim
+
+      # scala extensons
+      vim-scala
+
+      # theme
+      molokai
+      vim-airline
+    ];
+
+    extraConfig = builtins.readFile ./init.vim;
+
+    extraPackages = with pkgs; [
+      (python3.withPackages (ps: with ps; [
+        black
+        flake8
+      ]))
+      nodejs  # required for coc
+      haskell-language-server
+      rnix-lsp
+    ];
+    extraPython3Packages = (ps: with ps; [
+      jedi
+    ]);
+  };
+
+  xdg.configFile."nvim/coc-settings.json".text = builtins.readFile ./coc-settings.json;
+}
