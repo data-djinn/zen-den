7368347f336cf285375c4dc816fc7fd8fd08c6f9
data-djinn <data-djinn@pm.me>
2022-11-16 09:26:34 -0500

    modularize nvidia configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -3,17 +3,7 @@
 
 { inputs, lib, config, pkgs, ... }:
 
-let  # env vars required for finegrained
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-GO
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia::
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in
 {
-
   imports = [
     # If you want to use modules from other flakes (such as nixos-hardware), use something like:
     # inputs.hardware.nixosModules.common-cpu-amd
@@ -27,6 +17,7 @@ in
     ./hardware-configuration.nix
     # You can also split up your configuration and import pieces of it here.
     ./erase_your_darlings.nix
+    ./nvidia.nix
   ];
 
   # =========================
@@ -45,51 +36,6 @@ in
     supportedFilesystems = [ "btrfs" ];
   };
 
-  # =========================
-  # CUSTOM HARDWARE CONFIG
-  # =========================
-
-  # Install latest nvidia driver
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  # install shell script defined above
-  environment.systemPackages = [ nvidia-offload ];
-
-  # enable secondary monitors at boot time
-  specialisation = {
-    external-display.configuration = {
-      system.nixos.tags = [ "external-display" ];
-      hardware.nvidia = {
-        prime.offload.enable = lib.mkForce false;
-        powerManagement = {
-          enable = lib.mkForce false;
-          finegrained = lib.mkForce false;
-        };
-      };
-    };
-  };
-
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-  };
-
-  hardware.nvidia = {
-    prime = {
-      offload.enable = true;
-
-      # FIXME: nix-shell -p lshw --run "lshw -c display"
-      nvidiaBusId = "PCI:1:00:0";
-      intelBusId = "PCI:0:2:0";
-    };
-
-    powerManagement = {
-      enable = true;  # enable systemd-based graphical suspend to prevent black screen on resume
-      finegrained = true;  # power down GPU when no applications are running that require nvidia
-    };
-  };
-
-  # Misc. hardware config TODO: mv to hardware-configuration.nix?
   services.tlp.enable = true;
   sound.enable = true;
   hardware.pulseaudio.enable = true;
---
+++ nixos/nvidia.nix
@@ -0,0 +1,56 @@
+{pkgs,lib, ...}:
+
+let  # env vars required for finegrained
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-GO
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia::
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in
+{
+  # =========================
+  # CUSTOM HARDWARE CONFIG
+  # =========================
+
+  # Install latest nvidia driver
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  # install shell script defined above
+  environment.systemPackages = [ nvidia-offload ];
+
+  # enable secondary monitors at boot time
+  specialisation = {
+    external-display.configuration = {
+      system.nixos.tags = [ "external-display" ];
+      hardware.nvidia = {
+        prime.offload.enable = lib.mkForce false;
+        powerManagement = {
+          enable = lib.mkForce false;
+          finegrained = lib.mkForce false;
+        };
+      };
+    };
+  };
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+  };
+
+  hardware.nvidia = {
+    prime = {
+      offload.enable = true;
+
+      # FIXME: nix-shell -p lshw --run "lshw -c display"
+      nvidiaBusId = "PCI:1:00:0";
+      intelBusId = "PCI:0:2:0";
+    };
+
+    powerManagement = {
+      enable = true;  # enable systemd-based graphical suspend to prevent black screen on resume
+      finegrained = true;  # power down GPU when no applications are running that require nvidia
+    };
+  };
+}
