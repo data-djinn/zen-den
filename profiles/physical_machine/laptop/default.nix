cd45fe2b682c9bacfe90ed8a5c6a5dba15fdcf26
data-djinn <data-djinn@pm.me>
2023-01-17 23:15:23 -0500

    mv touchpad & networkmgr to general laptop profile

--- profiles/physical_machine/laptop/default.nix
+++ profiles/physical_machine/laptop/default.nix
@@ -4,9 +4,12 @@
   imports = [
     ../../common
     ../pc
+
+    ../../modules/system/devices/touchpad
     ../../../modules/services/location
   ];
 
   services.tlp.enable = true;
 
+  networking.networkmanager.enable = true;
 }
