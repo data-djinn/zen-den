20659c81d42fc3dc88af3141c8e418c306ea23de
data-djinn <data-djinn@pm.me>
2023-01-18 19:24:51 -0500

    configure laptop lock & hibernation settings

--- modules/profiles/physical_machine/laptop/default.nix
+++ modules/profiles/physical_machine/laptop/default.nix
@@ -10,5 +10,18 @@
 
   services.tlp.enable = true;
 
+  services.logind = {
+    killUserProcesses = false;  # on lock
+    lidSwitch = "suspend-then-hibernate";  # battery power
+    lidSwitchExternalPower = "lock";  # plugged in
+    lidSwitchDocked = "ignore"; # external display
+
+    # lock after 60 seconds of inactivity
+    extraConfig = "
+      IdleActionSec=60
+      IdleAction=lock
+      ";
+  };
+
   networking.networkmanager.enable = true;
 }
