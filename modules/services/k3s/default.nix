9982aadcfe7e9ae97bd920fd908c46b37064177f
data-djinn <data-djinn@pm.me>
2023-03-08 13:29:21 +0000

    add packages for rook deployment
    add packages for rook deployment

--- modules/services/k3s/default.nix
+++ modules/services/k3s/default.nix
@@ -4,5 +4,12 @@
   services.k3s.enable = true;
   services.k3s.role = "server";
   # services.k3s.extraFlags = toString [ "--TBD" ];
-  environment.systemPackages = [ pkgs.k3s ];
+  environment.systemPackages = with pkgs; [
+    k3s
+    lvm2
+    helm
+  ];
+
+  # Needed for rook
+  boot.kernelModules = [ "ceph" ];
 }
