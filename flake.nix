9c28140273952e2da7e66b93b14349936eb924c2
data-djinn <data-djinn@pm.me>
2024-09-12 21:40:29 -0400

    simplified flake.nix, point to unstable, remove NUR

--- flake.nix
+++ flake.nix
@@ -1,24 +1,20 @@
 {
-  description = "Your new nix config";
+  description = "djinn's homelab";
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hardware.url = "github:nixos/nixos-hardware";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = { nixpkgs, home-manager, hardware, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, hardware, ... }@inputs:
     let
+    inherit (self) outputs;
       forAllSystems = nixpkgs.lib.genAttrs [
         "aarch64-linux"
         "i686-linux"
@@ -26,55 +22,34 @@
         "aarch64-darwin"
         "x86_64-darwin"
       ];
-    in
-    rec {
+    in {
       # Your custom packages and modifications
       overlays = {
         default = import ./overlay { inherit inputs; };
       };
 
-      # Devshell for bootstrapping
-      # Accessible through 'nix develop' or 'nix-shell' (legacy)
-      devShells = forAllSystems (system: {
-        default = nixpkgs.legacyPackages.${system}.callPackage ./shell.nix { };
-      });
-
       # This instantiates nixpkgs for each system listed above
       # Allowing you to add overlays and configure it (e.g. allowUnfree)
-      # Our configurations will use these instances
-      # Your flake will also let you access your package set through nix build, shell, run, etc.
-      legacyPackages = forAllSystems (system:
-        import inputs.nixpkgs {
-          inherit system;
-          # This adds our overlays to pkgs
-          overlays = builtins.attrValues overlays;
-
-          # NOTE: Using `nixpkgs.config` in your NixOS config won't work
-          # Instead, you should set nixpkgs configs here
-          # (https://nixos.org/manual/nixpkgs/stable/#idm140737322551056)
-          config = {
-            allowUnfree = true;
-            # FIXME: NUR still not available 
-            packageOverrides = pkgs: {
-              nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-                inherit pkgs;
-              };
-            };
-          };
-        }
-      );
+      # packages= forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+      nixosModules = import ./modules/nixos;
+      homeManagerModules = import ./modules/home;
 
       nixosConfigurations = {
+        icarus = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; }; # Pass flake inputs to our config
+          modules = [
+            ./machines/icarus
+          ];
+        };
         obelisk = nixpkgs.lib.nixosSystem {
-          pkgs = legacyPackages.x86_64-linux;
-          specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+          specialArgs = { inherit inputs outputs; }; # Pass flake inputs to our config
           modules = [
             ./machines/obelisk
           ];
         };
         kraken = nixpkgs.lib.nixosSystem {
-          pkgs = legacyPackages.x86_64-linux;
-          specialArgs = { inherit inputs; };
+          specialArgs = { inherit inputs outputs; };
           modules = [
             hardware.nixosModules.common-cpu-intel-cpu-only
             ./machines/kraken
@@ -83,11 +58,10 @@
       };
 
       homeConfigurations = {
-        "djinn@kraken" = home-manager.lib.homeManagerConfiguration {
-          pkgs = legacyPackages.x86_64-linux;
-          extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        "djinn@icarus" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-linux; # home-manager requires pkgs instance
+          extraSpecialArgs = { inherit inputs outputs; };
           modules = [
-            # > Our main home-manager configuration file <
             ./modules/home
           ];
         };
